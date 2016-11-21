28126dfa3848afe5d8e6224fc3963826556cbf8e
Stuart Stanley <Stuart.Stanley@emc.com>
2016-11-21 15:54:08 -0600

    Fleshed out self-test to just verify my feel for the sequence that parts of nose.plugins.Plugin API members get called.

--- stream_monitor.py
+++ stream_monitor.py
@@ -5,32 +5,56 @@ import sys
 
 class StreamMonitorPlugin(Plugin):
     name = "stream-monitor"
+    def __init__(self, *args, **kwargs):
+        self.__save_call_sequence = None
+        self.__print_to = None
+        # Uncomment next line to view steps to console live
+        # self.__print_to = sys.stderr   
+        super(StreamMonitorPlugin, self).__init__(*args, **kwargs)
+
+    def _self_test_print_step_enable(self):
+        self.__save_call_sequence = []
+
+    def _self_test_sequence_seen(self):
+        rl = self.__save_call_sequence
+        self.__save_call_sequence = []
+        return rl
+
+    def __take_step(self, name, **kwargs):
+        if self.__save_call_sequence is not None:
+            self.__save_call_sequence.append((name, kwargs))
+
+        if self.__print_to is not None:
+            print >>self.__print_to, 'ESTEP: {0} {1}'.format(name, kwargs)
 
     def options(self, parser, env=os.environ):
-        print >>sys.stderr, "options"
+        self.__take_step('options', parser=parser, env=env)
         self.__log = logging.getLogger('nose.plugins.streammonitor')
         super(StreamMonitorPlugin, self).options(parser, env=env)
 
     def configure(self, options, conf):
-        print >>sys.stderr, "configure"
+        self.__take_step('configure', options=options, conf=conf)
         super(StreamMonitorPlugin, self).configure(options,conf)
         if not self.enabled:
             return
 
     def finalize(self, result):
-        print >>sys.stderr, "finalize"
+        self.__take_step('finalize', result=result)
         self.__log.info('Stream Monitor Active')
 
     def begin(self):
-        print >>sys.stderr, "begin"
+        self.__take_step('begin')
 
     def beforeTest(self, test):
-        print >>sys.stderr, "beforeTest", test
+        self.__take_step('beforeTest', test=test)
+
+    def afterTest(self, test):
+        self.__take_step('afterTest', test=test)
 
     def startTest(self, test):
-        print >>sys.stderr, "startTest", test
+        self.__take_step('startTest', test=test)
 
     def stopTest(self, test):
-        print >>sys.stderr, "stopTest", test
+        self.__take_step('stopTest', test=test)
 
         
